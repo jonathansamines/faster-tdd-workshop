8f803134a2c0e66049a6815241632a280afbed05
Jonathan Samines <jsamines@paypal.com>
2019-09-11 14:10:52 -0400

    [start_here] Integrate input infrastructure into application tests

--- src/_app_test.js
+++ src/_app_test.js
@@ -3,20 +3,39 @@
 
 const { expect } = require('chai');
 const App = require('./app.js');
+const CommandLine = require('./command-line');
 
 describe('App', function() {
 
-	it('encodes input using rot13 cypher', function() {
+	it('encodes null command line parameter using rot13 cypher', function() {
 		// arrange
-		const app = new App();
+		const cli = CommandLine.createNull('hello');
+		const app = new App(cli);
 
 		// act
 		const expected = 'uryyb';
-		const actual = app.transform('hello');
+		const actual = app.transform();
 
 		// assert
 		expect(actual).to.be.equal(expected);
 
 		// reset
+	});
+
+	it('encodes command line parameter using rot13 cypher', () => {
+		const oldArgs = process.argv;
+
+        try {
+            process.argv = ['node', 'my-file.js', 'hello'];
+
+			const app = new App();
+
+			const actual = app.transform();
+			const expected = 'uryyb';
+
+            expect(actual).to.be.equal(expected);
+        } finally {
+            process.argv = oldArgs;
+		}
 	});
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const rot13 = require('./rot13');
+const CommandLine = require('./command-line');
 
 module.exports = class Application {
-    transform(input) {
+    constructor(cli = CommandLine.create()) {
+        this._cli = cli;
+    }
+
+    transform() {
+        const input = this._cli.getArg();
+        
         return rot13.cypher(input);
     }
 };
